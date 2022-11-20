67436a153402d53225fe807bcf3c306db515e307
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-11-20 10:06:05 -0300

    feat: add routes for /order

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,52 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Listener port 3000');
+app.use(express.json());
+
+app.listen(3000);
+
+const orders = [];
+
+app.get('/order', (req, res) => {
+  res.send(orders)
 })
 
-app.get('/', (req, res) => {
-  res.json({
-    "message": "Hello World!"
-  })
+app.post('/order', (req, res) => {
+  orders.push({ id: uuid(), ...req.body})
+  res.send(orders)
+})
+
+app.put('/order/:id', (req, res) => {
+  const id = req.params.id;
+  let index;
+  
+  orders.forEach((order, i) => order.id === id ? index = i : []);
+  orders[index] = req.body;
+
+  res.send(`Updated order: ${id}`)
+})
+
+app.delete('/order/:id', (req, res) => {
+  const id = req.params.id;
+  let index;
+  
+  orders.forEach((order, i) => order.id === id ? index = i : []);
+  orders.splice(index, 1);
+
+  res.send(`Order Deleted: ${id}`);
+})
+
+app.patch('/order/:id', (req, res) => {
+  const id = req.params.id;
+  let index;
+  
+  if (orders.length !== 0) {
+    orders.forEach((order, i) => order.id === id ? index = i : []);
+    orders.push({...req.body, status: "Pronto"})
+
+    res.send(`Updated status: ${id}`)
+  }
+
+  res.send(`ID not found.`)
 })
